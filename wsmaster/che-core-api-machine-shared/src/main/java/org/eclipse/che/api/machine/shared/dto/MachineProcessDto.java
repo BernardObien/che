dc33f5d00c4ec4c660b3e0e4412c3382a25339c4
Valeriy Svydenko <vsvydenko@codenvy.com>
2016-07-25 15:17:28 +0300

    CHE-1785: extend CommandDto

--- wsmaster/che-core-api-machine-shared/src/main/java/org/eclipse/che/api/machine/shared/dto/MachineProcessDto.java
+++ wsmaster/che-core-api-machine-shared/src/main/java/org/eclipse/che/api/machine/shared/dto/MachineProcessDto.java
@@ -25,7 +25,7 @@ import java.util.Map;
  * @author andrew00x
  */
 @DTO
-public interface MachineProcessDto extends MachineProcess, Hyperlinks {
+public interface MachineProcessDto extends CommandDto, MachineProcess, Hyperlinks {
     void setPid(int pid);
 
     MachineProcessDto withPid(int pid);
@@ -52,8 +52,6 @@ public interface MachineProcessDto extends MachineProcess, Hyperlinks {
 
     @Override
     MachineProcessDto withLinks(List<Link> links);
-
-    void setAttributes(Map<String, String> attributes);
 
     MachineProcessDto withAttributes(Map<String, String> attributes);
 }
